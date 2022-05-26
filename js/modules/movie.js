0511e9f3da666ba7a41375a5cb894c81718ef36a
KenAniar <51713799+LittleBolt00816@users.noreply.github.com>
2022-05-27 04:18:51 +0800

    Finish Activity 3

--- js/modules/movie.js
+++ js/modules/movie.js
@@ -1,36 +1,60 @@
 import fetchData from "./fetchData.js";
+import config from "./config.js";
 const form = document.getElementById("search_form");
 const row = document.querySelector(".row");
 
-const renderMovie = async () => {
-   const { data } = await fetchData("upcoming");
+const IMAGE_BASE_URL = config.IMAGE_BASE_URL;
 
-   renderMovieList(data.results);
+const renderMovie = async () => {
+  const { data } = await fetchData("upcoming");
+
+  renderMovieList(data.results);
+  console.log(data.results);
 };
 
 export default renderMovie;
 
 const searchHandler = async (event) => {
-   event.preventDefault();
-   let query = document.getElementById("search").value;
-   const { data } = await fetchData("search", query);
+  event.preventDefault();
+  let query = document.getElementById("search").value;
+  const { data } = await fetchData("search", query);
 
-   if (!query) {
-      renderMovie();
-   }
-   renderMovieList(data.results);
+  if (!query) {
+    renderMovie();
+  }
+  renderMovieList(data.results);
 };
 
 form.addEventListener("submit", searchHandler);
 
 const renderMovieList = (data) => {
-   row.innerHTML = data
-      .map((movie) => {
-         return `
-            <a href="single-movie.html?movie=${movie.id}">
-                <h3>${movie.title}</h3>
+  row.innerHTML = data
+    .map((movie) => {
+      return `
+            <a href="single-movie.html?movie=${
+              movie.id
+            }" onclick="return false" ondblclick="location=this.href">
+            <div class="card">
+               <img class="card__image" src="${
+                 IMAGE_BASE_URL + movie.poster_path
+               }" alt="" />
+               <div class="card__details">
+                  <div class="card__content">
+                     <span class="card__header">Title</span>
+                     <h1 class="card__title ellipsis">${
+                       movie.title || "N/A"
+                     }</h1>
+                  </div>
+                  <div class="card__content">
+                     <span class="card__header">Release Date</span>
+                     <h2 class="card__genre ellipsis">${
+                       movie.release_date || "N/A"
+                     }</h2>
+                  </div>
+               </div>
+            </div>
             </a>
            `;
-      })
-      .join("");
+    })
+    .join("");
 };
--- js/modules/singleMovie.js
+++ js/modules/singleMovie.js
@@ -1,21 +1,94 @@
 import fetchData from "./fetchData.js";
-const row = document.querySelector(".row");
+import config from "./config.js";
+
+const row = document.querySelector(".movie-data-row");
+const rowSimilar = document.querySelector(".similar-movies-row");
+const IMAGE_BASE_URL = config.IMAGE_BASE_URL;
 
 const getSingleMovie = async () => {
-   const pathName = window.location.href.split("/")[3];
-   const movieId = pathName.split("?")[1].split("=")[1];
-   const { data } = await fetchData("single", "", movieId);
+  const pathName = window.location.href.split("/")[3];
+  const movieId = pathName.split("?")[1].split("=")[1];
+  const { data: singleData } = await fetchData("single", "", movieId);
+  const { data: similarData } = await fetchData("similar", "", movieId);
 
-   renderMovie(data);
+  renderMovie(singleData);
+  renderSimilarMovie(similarData.results);
 };
 
 const renderMovie = (data) => {
-   row.innerHTML = `
-                <a href="index.html">
-                back
-                </a>
-               <h3>${data.title}</h3>
+  row.innerHTML = `
+      <div class="single-movie__content">
+        <div class="single-movie__bg">
+          <img class="single-movie__bg-img" src="${
+            IMAGE_BASE_URL + data.backdrop_path
+          }" alt="" />
+        </div>
+        <div class="single-movie__main-info">
+          <div class="single-movie__poster">
+            <img
+              class="single-movie__poster-img"
+              src="${IMAGE_BASE_URL + data.poster_path}"
+              alt=""
+            />
+          </div>
+          <div class="single-movie__info">
+            <h1 class="single-movie__title">${data.title || "N/A"}</h1>
+            <div class="single-movie__other">
+              <span class="single-movie__label">Genre</span>
+              <span class="single-movie__value">${data.genres.map(
+                (item) => item.name || "N/A"
+              )}</span>
+            </div>
+            <div class="single-movie__other">
+              <span class="single-movie__label">Status</span>
+              <span class="single-movie__value">${data.status || "N/A"}</span>
+            </div>
+            <div class="single-movie__other">
+              <span class="single-movie__label">Release Date</span>
+              <span class="single-movie__value">${
+                data.release_date || "N/A"
+              }</span>
+            </div>
+          </div>
+        </div>
+        <div class="single-movie__story">
+          <h1 class="single-movie__story-label">Story</h1>
+          <p class="single-movie__story-content">
+           ${data.overview || "N/A"}
+          </p>
+        </div>
+      </div>
            `;
+};
+
+const renderSimilarMovie = (data) => {
+  rowSimilar.innerHTML = data
+    .map((movie) => {
+      return `
+          <a href="single-movie.html?movie=${
+            movie.id
+          }" onclick="return false" ondblclick="location=this.href">
+          <div class="card">
+             <img class="card__image" src="${
+               IMAGE_BASE_URL + movie.poster_path
+             }" alt="" />
+             <div class="card__details">
+                <div class="card__content">
+                   <span class="card__header">Title</span>
+                   <h1 class="card__title ellipsis">${movie.title || "N/A"}</h1>
+                </div>
+                <div class="card__content">
+                   <span class="card__header">Release Date</span>
+                   <h2 class="card__genre ellipsis">${
+                     movie.release_date || "N/A"
+                   }</h2>
+                </div>
+             </div>
+          </div>
+          </a>
+         `;
+    })
+    .join("");
 };
 
 getSingleMovie();
